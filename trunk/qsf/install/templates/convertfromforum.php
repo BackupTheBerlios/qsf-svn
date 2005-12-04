df47d50fa4b65c018489525798f9e9b0b94d6684
samson <samson@a80fa616-1103-0410-9c83-d7d84d2b2704>
2005-12-04 03:14:01 +0000

    Conversion for IPB 2.1.x added

--- trunk/qsf/install/templates/convertfromforum.php
+++ trunk/qsf/install/templates/convertfromforum.php
@@ -6,12 +6,14 @@
        <b>Select forum:</b>
        <select name='old_forum'>
         <option value=''>Forum</option>
+        <option value='convert_ipb21.php'>Invision Power Board 2.1</option>
         <option value='convert_ikon312a.php'>Ikonboard 3.12a</option>
         <option value='convert_invision131f.php'>Invisionboard 1.31f</option>
         <option value='convert_phpbb2.php'>phpBB2 2.x (any 2.x version)</option>
         <option value='convert_yabbse.php'>YaBB SE 1.5.5</option>
         <option value='convert_smf105.php'>Simple Machines Forum 1.05</option>
         <option value='convert_mybb.php'>MyBB 1.0 PR2</option>
+
        </select><br /><br />
        </td>
     </tr>
