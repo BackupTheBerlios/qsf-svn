5c3878593cae2884b7114e69b0cf4776d3857db1
geoff <geoff@a80fa616-1103-0410-9c83-d7d84d2b2704>
2006-03-07 11:11:45 +0000

    more work on html generation

--- trunk/qsf/tools/makehtml.sh
+++ trunk/qsf/tools/makehtml.sh
@@ -5,20 +5,23 @@
 
 cat htmldoc/header.html > ../docs/html/readme.html
 cat htmldoc/readmeheader.html >> ../docs/html/readme.html
-txt2tags --target=xhtml --no-headers --outfile=- ../docs/README.txt >> ../docs/html/readme.html
+perl reformatwiki.pl < ../docs/README.txt > README.txt
+txt2tags --target=xhtml --no-headers --outfile=- README.txt >> ../docs/html/readme.html
 cat htmldoc/footer.html >> ../docs/html/readme.html
 
 cat htmldoc/header.html > ../docs/html/faq.html
 cat htmldoc/faqheader.html >> ../docs/html/faq.html
-# Change * to - for proper bullet points
-perl  -p -e 's/^\* /- /g' ../docs/FAQ.txt > TestFAQ.txt
-# Make sure headers work
-perl  -p -e 's/^===/\n===/g' TestFAQ.txt > FAQ.txt
+perl reformatwiki.pl < ../docs/FAQ.txt > FAQ.txt
 txt2tags --target=xhtml --no-headers --outfile=- FAQ.txt >> ../docs/html/faq.html
 cat htmldoc/footer.html >> ../docs/html/faq.html
 
 cat htmldoc/header.html > ../docs/html/install.html
 cat htmldoc/installheader.html >> ../docs/html/install.html
-txt2tags --target=xhtml --no-headers --outfile=- ../docs/INSTALL.txt >> ../docs/html/install.html
+perl reformatwiki.pl < ../docs/INSTALL.txt > INSTALL.txt
+txt2tags --target=xhtml --no-headers --outfile=- INSTALL.txt >> ../docs/html/install.html
 cat htmldoc/footer.html >> ../docs/html/install.html
+
+rm README.txt
+rm FAQ.txt
+rm INSTALL.txt
 
