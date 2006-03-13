fd9acf89d348eb6683502fd178d2048f96c9752c
geoff <geoff@a80fa616-1103-0410-9c83-d7d84d2b2704>
2006-03-13 04:53:53 +0000

    Put in a base help html page

--- trunk/qsf/tools/makehtml.sh
+++ trunk/qsf/tools/makehtml.sh
@@ -3,6 +3,12 @@
 # Use txt2tags to build the html doc files. Use header.html and footer.html
 # in htmldoc to make them all pretty
 
+cat  htmldoc/header.html > ../docs/html/index.html
+cat htmldoc/indexheader.html >> ../docs/html/index.html
+perl reformatwiki.pl < htmldoc/index.txt > INDEX.txt
+txt2tags --target=xhtml --no-headers --outfile=- INDEX.txt >> ../docs/html/index.html
+cat htmldoc/footer.html >> ../docs/html/index.html
+
 cat htmldoc/header.html > ../docs/html/readme.html
 cat htmldoc/readmeheader.html >> ../docs/html/readme.html
 perl reformatwiki.pl < ../docs/README.txt > README.txt
@@ -21,6 +27,7 @@ perl reformatwiki.pl < ../docs/INSTALL.txt > INSTALL.txt
 txt2tags --target=xhtml --no-headers --outfile=- INSTALL.txt >> ../docs/html/install.html
 cat htmldoc/footer.html >> ../docs/html/install.html
 
+rm INDEX.txt
 rm README.txt
 rm FAQ.txt
 rm INSTALL.txt
