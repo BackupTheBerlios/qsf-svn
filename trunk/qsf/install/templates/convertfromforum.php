50e474f38a6ec2935a6af66d27c857fe2c1966ec
samson <samson@a80fa616-1103-0410-9c83-d7d84d2b2704>
2006-03-17 01:00:48 +0000

    Last minute version checks

--- trunk/qsf/install/templates/convertfromforum.php
+++ trunk/qsf/install/templates/convertfromforum.php
@@ -8,11 +8,11 @@
         <option value=''>Forum</option>
         <option value='convert_ipb21.php'>Invision Power Board 2.1</option>
         <option value='convert_vb351.php'>vBulliten 3.5.1</option>
-        <option value='convert_ikon312a.php'>Ikonboard 3.12a</option>
+        <option value='convert_ikon312a.php'>Ikonboard 3.1x</option>
         <option value='convert_invision131f.php'>Invisionboard 1.31f</option>
         <option value='convert_phpbb2.php'>phpBB2 2.x (any 2.x version)</option>
         <option value='convert_yabbse.php'>YaBB SE 1.5.5</option>
-        <option value='convert_smf105.php'>Simple Machines Forum 1.05</option>
+        <option value='convert_smf105.php'>Simple Machines Forum 1.05/1.06</option>
         <option value='convert_mybb.php'>MyBB 1.0/1.1</option>
         <option value='convert_xmb.php'>XMB 1.9</option>
 
