5327fca711f76ba2ad4934535e124707372e8576
samson <samson@a80fa616-1103-0410-9c83-d7d84d2b2704>
2006-06-20 03:28:44 +0000

    Clarified SMF option in conversions

--- trunk/qsf/install/templates/convertfromforum.php
+++ trunk/qsf/install/templates/convertfromforum.php
@@ -12,7 +12,7 @@
         <option value='convert_invision131f.php'>Invisionboard 1.31f</option>
         <option value='convert_phpbb2.php'>phpBB2 2.x (any 2.x version)</option>
         <option value='convert_yabbse.php'>YaBB SE 1.5.5</option>
-        <option value='convert_smf105.php'>Simple Machines Forum 1.05/1.06</option>
+        <option value='convert_smf105.php'>Simple Machines Forum 1.0x</option>
         <option value='convert_mybb.php'>MyBB 1.0/1.1</option>
         <option value='convert_xmb.php'>XMB 1.9</option>
 
