f004e795ccaf5df7dcc81647bdf7b75085df5905
geoff <geoff@a80fa616-1103-0410-9c83-d7d84d2b2704>
2006-09-14 01:50:17 +0000

    [security] Could run nastyness tthrough include

--- trunk/qsf/lib/activeutil.php
+++ trunk/qsf/lib/activeutil.php
@@ -15,6 +15,11 @@
  * GNU General Public License for more details.
  *
  **/
+ 
+if (!defined('QUICKSILVERFORUMS')) {
+	header('HTTP/1.0 403 Forbidden');
+	die;
+}
 
 require_once $set['include_path'] . '/lib/bbcode.php';
 
