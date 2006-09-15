83b71ff96561d0f35df0fa478ef5b8460840aeb1
samson <samson@a80fa616-1103-0410-9c83-d7d84d2b2704>
2006-09-15 02:32:08 +0000

    [Security] Further enhancements to the security fix

--- trunk/qsf/func/constants.php
+++ trunk/qsf/func/constants.php
@@ -20,6 +20,11 @@
  *
  **/
 
+if (!defined('QUICKSILVERFORUMS')) {
+	header('HTTP/1.0 403 Forbidden');
+	die;
+}
+
 /* Error Reporting */
 define('QUICKSILVER_NOTICE', 3);
 define('QUICKSILVER_ERROR', 5);
