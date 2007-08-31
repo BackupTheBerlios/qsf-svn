cff9054bc967fbc0e8087befcdf693ed958012ce
tx2rx <tx2rx@a80fa616-1103-0410-9c83-d7d84d2b2704>
2007-08-31 23:11:30 +0000

    Undo accidental settings commit (oops)

--- trunk/qsf/settings.php
+++ trunk/qsf/settings.php
@@ -1,18 +1,18 @@
 <?php
-$set = array();
 
 if (!defined('QUICKSILVERFORUMS')) {
-       header('HTTP/1.0 403 Forbidden');
-       die;
+	header('HTTP/1.0 403 Forbidden');
+	die;
 }
 
+$set = array();
 $set['db_host'] = 'localhost';
-$set['db_name'] = 'qsf';
-$set['db_pass'] = 'qsf';
-$set['db_port'] = '5432';
+$set['db_name'] = '';
+$set['db_pass'] = '';
+$set['db_port'] = '3306';
 $set['db_socket'] = '';
-$set['db_user'] = 'qsf';
-$set['dbtype'] = 'pgsql';
+$set['db_user'] = '';
+$set['dbtype'] = 'mysql';
+$set['installed'] = '0';
 $set['prefix'] = 'qsf_';
-$set['installed'] = '1';
 ?>
