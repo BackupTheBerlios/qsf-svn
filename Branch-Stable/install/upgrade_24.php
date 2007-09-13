4acfce6d2c60cf7bf087eaad736a2c4b3133c5e3
samson <samson@a80fa616-1103-0410-9c83-d7d84d2b2704>
2007-09-13 20:06:56 +0000

    Attachment data missing from the posting form. --Matt

--- Branch-Stable/install/upgrade_24.php
+++ Branch-Stable/install/upgrade_24.php
@@ -30,7 +30,8 @@ if (!defined('INSTALLER')) {
 $need_templates = array(
 	// Added templates
 	// Changed templates
-	'TOPIC_QUICKREPLY'
+	'TOPIC_QUICKREPLY',
+	'POST_ATTACH_REMOVE'
 	);
 
 // Permission changes	
