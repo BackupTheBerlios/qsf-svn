99013091094034b32d2f56aec8e0152f021031d9
samson <samson@a80fa616-1103-0410-9c83-d7d84d2b2704>
2007-09-24 02:59:00 +0000

    Define a user agent for the RSS reader.

--- Branch-Stable/modlets/rssreader.php
+++ Branch-Stable/modlets/rssreader.php
@@ -57,6 +57,8 @@ class rssreader extends modlet
 	 **/
 	function run($param)
 	{
+		ini_set( 'user_agent', 'qsf-rssreader' );
+
 		if (!function_exists('xml_parser_create')) {
 			return "<!-- XML functions not enabled -->";
 		}
