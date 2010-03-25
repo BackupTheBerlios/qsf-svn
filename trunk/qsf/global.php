8ea0c8a38afb72411f7a8a03d4c914756eeaca4e
tx2rx <tx2rx@a80fa616-1103-0410-9c83-d7d84d2b2704>
2010-03-25 22:18:37 +0000

    [BUG 1431] Fixed sanitisation of lang parameter when OS's file seperator is \

--- trunk/qsf/global.php
+++ trunk/qsf/global.php
@@ -319,7 +319,7 @@ class qsfglobal
 			$lang = $this->get['lang'];
 		}
 
-		if (strstr($lang, '/') || !file_exists($path . 'languages/' . $lang . '.php')) {
+		if (strstr($lang, '\\') || strstr($lang, '/') || !file_exists($path . 'languages/' . $lang . '.php')) {
 			$lang = 'en';
 		}
 
